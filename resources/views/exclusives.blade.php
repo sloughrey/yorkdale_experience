72b1752ba39c7f38feb1d073fe442602aec63c1d
Sean Loughrey <s.a.loughrey@gmail.com>
2018-04-22 02:07:40 -0400

    exclusives page design in place

--- resources/views/exclusives.blade.php
+++ resources/views/exclusives.blade.php
@@ -1,19 +1,147 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12"><?php 
-       	foreach($exclusive_products as $p)
-       	{
-       		echo 'name: ' . $p['name'] . '<br>';
-       		echo 'qty remaining: ' . $p['qty_remaining'] . '<br>';
-       		echo 'qty made: ' . $p['qty_made'] . '<br>';
-       		echo 'price: ' . $p['price'] . '<br>';
-       		?> <img style="width:200px; height:200px;" src="<?php echo $p['image_url']; ?>" /><?php
-       		echo '<br><br><hr><br>';
-       	} ?>
+<div class="pos-f-t">
+        <nav style="width: 100%" class="wholeflexnav navbar navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a href="/home"><img id="navLogo" src="{{ asset('images/navlogo.png') }}"></a>
+            <a href=""><img src="{{ asset('images/icons/Shop-icon.png') }}"></a>
+        </nav>
+                <div class="collapse" id="navbarToggleExternalContent">
+                  <div style="height: 50vh;" class="bg-dark p-4 navFlex">
+                    <a style="border-bottom: solid #706a6ac0 1px;" class="navlink" href="#">
+                        <h4 class="text-white">Home</h4>
+                    </a>
+                    <a style="border-bottom: solid #706a6ac0 1px;" class="navlink" href="#">
+                        <h4 class="text-white">Map</h4>
+                    </a>
+                    <a class="navlink" href="#">
+                        <h4 class="text-white">My Account</h4>
+                    </a>
+                  </div>
+                </div>
+            </div>    
+ 
+        </div>
+
+    <div style="width:100%; margin: 0; padding: 10px 20px 0 20px;" class="text-center row">
+        <a class="col-3 subnavicons">
+            <img src="{{ asset('images/icons/search.png') }}">
+        </a>
+        <a class="col-3 subnavicons">
+            <img src="{{ asset('images/icons/events.png') }}">
+        </a>
+        <a class="col-3 subnavicons">
+            <img src="{{ asset('images/icons/exclusive.png') }}">
+        </a>
+        <a class="col-3 subnavicons">
+            <img src="{{ asset('images/icons/map.png') }}">
+        </a>
+    </div>
+
+    <div class="row">
+        <div style="padding-right: 0;" class="text-center col-12">
+            <h3 style="font-weight: 900;" class="tc mt-4 mb-4">EXCLUSIVE PRODUCTS</h3>
         </div>
     </div>
-</div>
+
+    <div class="row">
+            <div id="accordion" class="w-100" style="margin: 0 10%;" role="tablist">
+                    <!-- COACH START -->
+                    <div class="card">
+                      <div class="card-header" role="tab" id="headingOne">
+                        <h5 class="mb-0">
+                          <a data-toggle="collapse" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            COACH
+                          </a>
+                        </h5>
+                      </div>
+                  
+                      <div id="collapseOne" class="collapse show" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+                        <div class="tc card-body row">
+                            <div class="col-6">
+                                <img src="{{ asset('images/products/coach.png') }}">
+                            </div>
+                            <div class="flexBox col-6">
+                                <h5 class="f5"><strong>CANADA EXCLUSIVE</strong></h5>
+                                <p class="f7">ONLY <span class="warning"><strong>37 REMAINING</strong></span></p>
+                                <div style="margin-bottom: 7px;" class="buynow exclusiveButton">BUY NOW ></div>
+                                <div class="reserver exclusiveButton">RESERVE IN STORE></div>
+                            </div>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- CLAIRS START -->
+                    <div class="card">
+                      <div class="card-header" role="tab" id="headingTwo">
+                        <h5 class="mb-0">
+                          <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            HUGO BOSS
+                          </a>
+                        </h5>
+                      </div>
+                      <div id="collapseTwo" class="collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
+                            <div class="tc card-body row">
+                                <div class="col-6">
+                                    <img src="{{ asset('images/products/hugo.png') }}">
+                                </div>
+                                <div class="flexBox col-6">
+                                    <h5 class="f5"><strong>CANADA EXCLUSIVE</strong></h5>
+                                    <p class="f7">ONLY <span class="warning"><strong>1 REMAINING</strong></span></p>
+                                    <div style="margin-bottom: 7px;" class="buynow exclusiveButton">BUY NOW ></div>
+                                    <div class="reserver exclusiveButton">RESERVE IN STORE></div>
+                                </div>
+                            </div>
+                      </div>
+                    </div>
+                    <!-- HOLT START -->
+                    <div class="card">
+                      <div class="card-header" role="tab" id="headingThree">
+                        <h5 class="mb-0">
+                          <a class="collapsed" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                HOLT RENFREW
+                          </a>
+                        </h5>
+                      </div>
+                      <div id="collapseThree" class="collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#accordion">
+                            <div class="tc card-body row">
+                                <div class="col-6">
+                                    <img src="{{ asset('images/products/holt.png') }}">
+                                </div>
+                                <div class="flexBox col-6">
+                                    <h5 class="f5"><strong>CANADA EXCLUSIVE</strong></h5>
+                                    <p class="f7">ONLY <span class="warning"><strong>5 REMAINING</strong></span></p>
+                                    <div style="margin-bottom: 7px;" class="buynow exclusiveButton">BUY NOW ></div>
+                                       <div class="reserver exclusiveButton">RESERVE IN STORE></div>
+                                </div>
+                            </div>
+                      </div>
+                    </div>
+                    <!-- PEOPLES -->
+                    <div class="card">
+                        <div class="card-header" role="tab" id="headingFour">
+                          <h5 class="mb-0">
+                            <a class="collapsed" data-toggle="collapse" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                                PEOPLES
+                            </a>
+                          </h5>
+                        </div>
+                        <div id="collapseFour" class="collapse" role="tabpanel" aria-labelledby="headingFour" data-parent="#accordion">
+                            <div class="tc card-body row">
+                                <div class="col-6">
+                                    <img src="{{ asset('images/products/peoples.png') }}">
+                                </div>
+                                <div class="flexBox col-6">
+                                       <h5 class="f5"><strong>CANADA EXCLUSIVE</strong></h5>
+                                       <p class="f7">ONLY <span class="warning"><strong>12 REMAINING</strong></span></p>
+                                    <div style="margin-bottom: 7px;" class="buynow exclusiveButton">BUY NOW ></div>
+                                    <div class="reserver exclusiveButton">RESERVE IN STORE></div>
+                                </div>
+                            </div>
+                        </div>
+                       </div>
+                  </div>
+    </div>
 @endsection
