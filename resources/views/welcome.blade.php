aa536a51b895c18074b693fa6d623c3b3aebc880
Sean Loughrey <s.a.loughrey@gmail.com>
2018-04-21 22:24:26 -0400

    Setting up landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,16 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
-                <div class="row flexBox">
-			        <div style="height: 100px; width: 100%"></div>
-			        <div id="welcome" style="" class="center col-12">
-			            <p>Welcome</p>
-			            <p>to the</p>
-			            <img style="width: 220px;" src="{{ URL::asset('images/yorkdale_logo.png') }}"/>
-			            <p>experience</p>
-			        </div>
-    			</div>
-            </div>
+			<div class="row">
+        		<div id="homepage" style="background-image: url({{ asset('images/home-page.png') }})"></div>
+    		</div>
+	    	<div class="row">
+	            <div class="flexBox center col 12">
+	                <button style="background-color: #366EA7" class="logbutton hvr-rectangle-out-blue">LOGIN WITH FACEBOOK</button>
+	                <br>
+	                <button style="background-color: #BD282E" class="logbutton hvr-rectangle-out-red">LOGIN WITH EMAIL</button>
+	            </div>
+	        </div>
         </div>
     </div>
 </div>
