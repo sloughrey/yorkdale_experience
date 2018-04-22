52940da0ba4f4cd15069edb434d18365149b6c29
Sean Loughrey <s.a.loughrey@gmail.com>
2018-04-21 21:03:46 -0400

    added sample exclusive data and images

--- resources/views/exclusives.blade.php
+++ resources/views/exclusives.blade.php
@@ -3,8 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
-        This is the exclusives pages
+        <div class="col-md-12"><?php 
+       	foreach($exclusive_products as $p)
+       	{
+       		echo 'name: ' . $p['name'] . '<br>';
+       		echo 'qty remaining: ' . $p['qty_remaining'] . '<br>';
+       		echo 'qty made: ' . $p['qty_made'] . '<br>';
+       		echo 'price: ' . $p['price'] . '<br>';
+       		echo 'img url: ' . $p['image_url'] . '<br><br><br>';
+       	} ?>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,48 @@ Route::get('/map', function(){
 })->name('map');
 
 Route::get('/exclusives',  function(){
-	return view('exclusives');
+	$exclusive_products = [];
+	$exclusive_products[] = [
+			'name' => 'Coach Dragonscale Handbag',
+			'price' => '2299.99',
+			'image_url' => URL::asset('images/coach_exclusive.jpeg'),
+			'qty_remaining' => 5,
+			'qty_made' => 100
+	];
+	
+	$exclusive_products[] = [
+			'name' => 'Future Proof Trainers',
+			'price' => '279.99',
+			'image_url' => URL::asset('images/nike_exclusive.jpeg'),
+			'qty_remaining' => 23,
+			'qty_made' => 500
+	];
+	
+	$exclusive_products[] = [
+			'name' => 'Burberry Signature Scarf',
+			'price' => '529.99',
+			'image_url' => URL::asset('images/burberry_exclusive.jpeg'),
+			'qty_remaining' => 63,
+			'qty_made' => 200
+	];/* 
+	
+	$exclusive_products[] = [
+			'name' => 'Coach Dragonscale Handbag',
+			'price' => '2299.99',
+			'image_url' => '',
+			'qty_remaining' => 5,
+			'qty_made' => 100
+	];
+	
+	$exclusive_products[] = [
+			'name' => 'Coach Dragonscale Handbag',
+			'price' => '2299.99',
+			'image_url' => '',
+			'qty_remaining' => 5,
+			'qty_made' => 100
+	]; */
+	
+	return view('exclusives', ['exclusive_products' => $exclusive_products]);
 })->name('exclusives');
 
 Route::get('/events', function(){
